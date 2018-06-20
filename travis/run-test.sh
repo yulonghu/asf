93aabd0b95487d8bea95af318de6d5c3181e553e
Jiapeng Fan <fanjiapeng@126.com>
2018-06-20 14:12:26 +0800

    install update

--- travis/run-test.sh
+++ travis/run-test.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-cp -r ./tests ./asf/
-cd ./asf && make test
+
+# Environment
+PHP_TEST_PATH="./tests"
+PHP_TO_PATH="./asf"
+
+# Check Test Directory
+if [ -d ${PHP_TO_PATH}/${PHP_TEST_PATH:2} ]; then
+    rm -rf ${PHP_TO_PATH}/${PHP_TEST_PATH:2}
+fi
+
+# Copy Test Directory
+cp -r ${PHP_TEST_PATH} ${PHP_TO_PATH}
+
+# Make test
+cd ${PHP_TO_PATH} && make test
